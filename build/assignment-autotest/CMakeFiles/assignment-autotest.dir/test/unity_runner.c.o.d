40ed5b18a32cb383b8acfe7dc1652d013400d3d9
4trastos <info@4trastos.com>
2025-07-20 01:53:14 +0200

    Resuelve conflictos

---
+++ build/assignment-autotest/CMakeFiles/assignment-autotest.dir/test/unity_runner.c.o.d
@@ -0,0 +1,3 @@
+assignment-autotest/CMakeFiles/assignment-autotest.dir/test/unity_runner.c.o: \
+ /home/davgalle/Documentos/aesd/repo/assignment-2-4trastos/assignment-autotest/test/unity_runner.c \
+ /usr/include/stdc-predef.h
