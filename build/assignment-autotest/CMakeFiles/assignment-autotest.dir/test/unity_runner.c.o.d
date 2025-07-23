a880eac99d708dea1170032b41d30b7892e6be18
4trastos <info@4trastos.com>
2025-07-23 17:53:54 +0200

    Cleanup: remove build artifacts and ignore build directory

--- build/assignment-autotest/CMakeFiles/assignment-autotest.dir/test/unity_runner.c.o.d
+++
@@ -1,3 +0,0 @@
-assignment-autotest/CMakeFiles/assignment-autotest.dir/test/unity_runner.c.o: \
- /home/davgalle/Documentos/aesd/repo/assignments-3-and-later-4trastos/assignment-autotest/test/unity_runner.c \
- /usr/include/stdc-predef.h
